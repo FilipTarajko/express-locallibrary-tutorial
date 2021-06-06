3301df82d8f38bf9ed243cfc9708725d089b0e09
Filip Tarajko <filiptarajko@gmail.com>
2021-06-06 13:34:57 +0200

    complete Part 2 challenge

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/nice', function(req, res, next) {
+  res.send('This is nice');
+});
+
 module.exports = router;
